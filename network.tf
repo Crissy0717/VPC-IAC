96d935b0d0985d0387874152abc111af80da4838
Crissy <crissyjohnson0717@gmail.com>
2023-08-29 17:59:21 -0400

    addingsubnet module

--- network.tf
+++ network.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "crissy-vpc" {
     Name = "crissy-vpc"
   }
 }
+resource "aws_subnet" "crissy-vpc-public-subnet" {
+  vpc_id     = aws_vpc.crissy-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+
+  tags = {
+    Name = "crissy-vpc"
+  }
+}
