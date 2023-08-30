18d6e45363a8568e993e0aa1ad9825070c97a65d
Crissy <crissyjohnson0717@gmail.com>
2023-08-30 17:47:22 -0400

    added default for all subnets

--- variables.tf
+++ variables.tf
@@ -9,14 +9,17 @@ variable "enviroment" {
 
 variable "vpc_cidr" {
   description = "vpc cidr block"
+  default = "10.0.0.0/16"
 }
 
 variable "crissy-vpc_public_subnet_1_cidr" {
   description = "public subnet 1 cidr block"
+  default = "10.0.1.0/24"
 }
 
 variable "crissy-vpc_public_subnet_2_cidr" {
   description = "Public Subnet 2 cidr block"
+  default = "10.0.2.0/24"
 }
 
 variable "crissy-vpc_public_subnet_3_cidr" {
@@ -26,12 +29,15 @@ variable "crissy-vpc_public_subnet_3_cidr" {
 
 variable "crissy-vpc_private_subnet_1_cidr" {
   description = "Private Subnet 1 cidr block"
+  default = "10.0.10.0/24"
 }
 
 variable "crissy-vpc_private_subnet_2_cidr" {
   description = "Private Subnet 2 cidr block"
+  default = "10.0.12.0/24"
 }
 
 variable "crissy-vpc_private_subnet_3_cidr" {
   description = "Private Subnet 3 cidr block"
+  default = "10.0.13.0/24"
 }
