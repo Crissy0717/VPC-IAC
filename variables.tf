5406144be9a64d8379107b41549b146908815051
Crissy <crissyjohnson0717@gmail.com>
2023-08-30 17:31:31 -0400

    added changes

--- variables.tf
+++ variables.tf
@@ -11,26 +11,26 @@ variable "vpc_cidr" {
   description = "vpc cidr block"
 }
 
-variable "public_subnet_1_cidr" {
+variable "crissy-vpc_public_subnet_1_cidr" {
   description = "public subnet 1 cidr block"
 }
 
-variable "public_subnet_2_cidr" {
+variable "crissy-vpc_public_subnet_2_cidr" {
   description = "Public Subnet 2 cidr block"
 }
 
-variable "public_subnet_3_cidr" {
+variable "crissy-vpc_public_subnet_3_cidr" {
   description = "Public Subnet 3 cidr block"
 }
 
-variable "private_subnet_1_cidr" {
+variable "crissy-vpc_private_subnet_1_cidr" {
   description = "Private Subnet 1 cidr block"
 }
 
-variable "private_subnet_2_cidr" {
+variable "crissy-vpc_private_subnet_2_cidr" {
   description = "Private Subnet 2 cidr block"
 }
 
-variable "private_subnet_3_cidr" {
+variable "crissy-vpc_private_subnet_3_cidr" {
   description = "Private Subnet 3 cidr block"
 }
