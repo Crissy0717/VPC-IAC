8b94c780a29365ca04ec7f97de9266b70300710f
Crissy <crissyjohnson0717@gmail.com>
2023-08-30 16:45:14 -0400

    more changes

--- variables.tf
+++ variables.tf
@@ -6,3 +6,31 @@ variable "region" {
 variable "enviroment" {
   default = "development"
 }
+
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "public subnet 1 cidr block"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "Public Subnet 2 cidr block"
+}
+
+variable "public_subnet_3_cidr" {
+  description = "Public Subnet 3 cidr block"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "Private Subnet 1 cidr block"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "Private Subnet 2 cidr block"
+}
+
+variable "private_subnet_3_cidr" {
+  description = "Private Subnet 3 cidr block"
+}
